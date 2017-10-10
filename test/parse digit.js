0f28bf98cab702f09cb2d8714dce11fca5b5be3b
Nikolay <halt.hammerzeit.at@gmail.com>
2017-10-11 02:59:10 +0300

    Added test for arabic digits

--- test/parse digit.js
+++ test/parse digit.js
@@ -9,5 +9,7 @@ describe('parse digit', function()
 		expect(parse_digit('')).to.be.undefined
 		expect(parse_digit('a')).to.be.undefined
 		expect(parse_digit('5')).to.equal('5')
+		// Arabic digits
+		expect(parse_digit('١')).to.equal('1')
 	})
 })
