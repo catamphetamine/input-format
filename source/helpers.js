038ce921f77333eb247f50cfb43cb0a62aa60517
catamphetamine <purecatamphetamine@gmail.com>
2019-11-09 02:35:34 +0300

    Code style

--- source/helpers.js
+++ source/helpers.js
@@ -1,8 +1,6 @@
 // Counts all occurences of a symbol in a string
-export function count_occurences(symbol, string)
-{
+export function count_occurences(symbol, string) {
 	let count = 0
-
 	// Using `.split('')` here instead of normal `for ... of`
 	// because the importing application doesn't neccessarily include an ES6 polyfill.
 	// The `.split('')` approach discards "exotic" UTF-8 characters
@@ -10,13 +8,10 @@ export function count_occurences(symbol, string)
 	// but template placeholder characters don't fall into that range
 	// so skipping such miscellaneous "exotic" characters
 	// won't matter here for just counting placeholder character occurrences.
-	for (const character of string.split(''))
-	{
-		if (character === symbol)
-		{
+	for (const character of string.split('')) {
+		if (character === symbol) {
 			count++
 		}
 	}
-
 	return count
 }
